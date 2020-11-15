c5b00dc5238a69ff0fd95a4bb0f5721551c80ceb
arihant jain <arihantjain812000@gmail.com>
2020-11-15 13:39:12 +0530

    semi final

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React ,{useEffect , Fragment}from "react";
 import  AOS from "aos";
 import "aos/dist/aos.css";
+
 import { Container,Row,Col, Card} from "react-bootstrap";
 import './Home.css';
 const Home=()=>{
+  const data=[{
+    name:"Arihant"
+  }]
     useEffect(() => {
         AOS.init({
           duration : 2000
         });
         AOS.refresh();
+       
       }, []);
       
    
